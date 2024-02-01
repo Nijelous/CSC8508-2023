474ccaf3ae4988a88ab33d5e0a01e793c5998e08
Alexander Fall <alexfallguy@gmail.com>
2024-02-01 14:21:48 +0000

    Implement basic variables and functionality
    Added in all the currently required/requested variables for the LevelManager.
    Added in functionality for a basic initialisation of the LevelManager, as well as of the GetDistanceToCamera function.

--- CSC8503CoreClasses/LevelManager.cpp
+++ CSC8503CoreClasses/LevelManager.cpp
@@ -1,1 +1,20 @@
 #include "LevelManager.h"
+
+using namespace NCL::CSC8503;
+
+LevelManager::LevelManager() {
+	levelList = std::vector<Level>();
+	roomList = std::vector<Room>();
+	activeLevel = -1;
+	playerPosition = Vector3(0, 0, 0);
+}
+
+void LevelManager::UpdateLevel() {
+}
+
+void LevelManager::LoadLevel(int id) {
+}
+
+float LevelManager::GetDistanceToCamera(Vector3& objectPosition) {
+	return (objectPosition - playerPosition).Length();
+}
