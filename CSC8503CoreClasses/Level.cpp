31bfa009dd1992293cd0a233db54ae8b1cd83f5e
Alexander Fall <alexfallguy@gmail.com>
2024-02-01 18:29:00 +0000

    Add Some Generic Level Functionality
    The majority of the level functionality has been added, requiring GameObjects to be properly implemented.

--- CSC8503CoreClasses/Level.cpp
+++ CSC8503CoreClasses/Level.cpp
@@ -1,1 +1,19 @@
 #include "Level.h"
+
+using namespace NCL::CSC8503;
+
+Level::Level(int levelID) {
+	mLevelID = levelID;
+	//std::map<Vector3, Tile> mTileMap;
+	mRoomList = std::map<Vector3, Room>();
+	mGuardPaths = std::vector<std::vector<Vector3>>();
+	mGuardCount = 0;
+	mCCTVPositions = std::vector<Vector3>();
+	mCCTVCount = 0;
+	mPrisonPosition = Vector3(0, 0, 0);
+	mPlayerStartPositions = std::make_unique_for_overwrite<Vector3[]>(4);
+	//mLights = std::vector<Vector3>();
+	//NavMesh
+	//mItemPositions = std::vector<Vector3>();
+	//std::vector<Vent> mVents;
+}
--- CSC8503CoreClasses/Level.h
+++ CSC8503CoreClasses/Level.h
@@ -1,8 +1,31 @@
 #pragma once
+#include "Room.h"
+using namespace NCL::Maths;
+
 namespace NCL {
 	namespace CSC8503 {
 		class Level {
-
+		public:
+			Level(int levelID);
+			~Level() {}
+			int GetGuardCount() const { return mGuardCount; }
+			int GetCCTVCount() const { return mCCTVCount; }
+			Vector3 GetPrisonPosition() const { return mPrisonPosition; }
+			Vector3 GetPlayerStartPosition(int player) { return mPlayerStartPositions[player]; }
+		protected:
+			int mLevelID;
+			//std::map<Vector3, Tile> mTileMap;
+			std::map<Vector3, Room> mRoomList;
+			std::vector<std::vector<Vector3>> mGuardPaths;
+			int mGuardCount;
+			std::vector<Vector3> mCCTVPositions;
+			int mCCTVCount;
+			Vector3 mPrisonPosition;
+			std::unique_ptr<Vector3[]> mPlayerStartPositions;
+			//std::vector<Vector3> mLights;
+			//NavMesh
+			//std::vector<Vector3> mItemPositions;
+			//std::vector<Vent> mVents;
 		};
 	}
 }
