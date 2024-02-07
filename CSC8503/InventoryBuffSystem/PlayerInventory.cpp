233ba12b9e8ef525f7b93632d47298fa699a7ba4
KyriakosNU <149068759+KyriakosNU@users.noreply.github.com>
2024-02-07 11:28:43 +0000

    Player Inventory Base

---
+++ CSC8503/InventoryBuffSystem/PlayerInventory.cpp
@@ -0,0 +1,93 @@
+	#include "PlayerInventory.h"
+
+	using namespace InventoryBuffSystem;
+
+	void PlayerInventory::Init()
+	{
+		for (int playerNo = 0; playerNo < 4; playerNo++)
+		{
+			for (int invSlot = 0; invSlot < 2; invSlot++)
+			{
+				mPlayerInventory[playerNo][invSlot] = none;
+			}
+		}
+
+	}
+
+	void PlayerInventory::AddItemToPlayer(item inItem, int playerNo)
+	{
+		for (int invSlot = 0; invSlot < 2; invSlot++)
+		{
+			if (mPlayerInventory[playerNo][invSlot] == none)
+			{
+				mPlayerInventory[playerNo][invSlot] = inItem;
+
+				if (mOnItemAddedFunctionMap.find(inItem) != mOnItemAddedFunctionMap.end())
+				{
+					mOnItemAddedFunctionMap[inItem](playerNo);
+				}
+				return;
+			}
+		}
+	}
+
+	void PlayerInventory::DropItemFromPlayer(item inItem, int playerNo)
+	{
+		for (int invSlot = 0; invSlot < MAX_INVENTORY_SLOTS; invSlot++)
+		{
+			if (mPlayerInventory[playerNo][invSlot] == inItem)
+			{
+				CreateItemPickup(inItem, (mPlayerGameObjectsPTR + playerNo)->GetTransform().GetPosition());
+
+				if (mOnItemDroppedFunctionMap.find(inItem) != mOnItemDroppedFunctionMap.end())
+				{
+					mOnItemDroppedFunctionMap[inItem](playerNo);
+				}
+
+				mPlayerInventory[playerNo][invSlot] = none;
+			}
+		}
+	}
+
+	void PlayerInventory::DropItemFromPlayer(int playerNo, int invSlot)
+	{
+		CreateItemPickup(mPlayerInventory[playerNo][invSlot],
+			(mPlayerGameObjectsPTR + playerNo)->GetTransform().GetPosition());
+
+		if (mOnItemDroppedFunctionMap.find(mPlayerInventory[playerNo][invSlot]) != mOnItemDroppedFunctionMap.end())
+		{
+			mOnItemDroppedFunctionMap[mPlayerInventory[playerNo][invSlot]](playerNo);
+		}
+
+		mPlayerInventory[playerNo][invSlot] = none;
+	}
+
+
+	void PlayerInventory::DropFlagFromPlayer(int playerNo)
+	{
+		for (int invSlot = 0; invSlot < MAX_INVENTORY_SLOTS; invSlot++)
+		{
+			if (mPlayerInventory[playerNo][invSlot] == flag)
+			{
+				CreateItemPickup(flag, *mFlagLocationPTR);
+				mPlayerInventory[playerNo][invSlot] = none;
+			}
+		}
+	}
+
+	void PlayerInventory::UseItemInPlayerSlot(int playerNo, int invSlot)
+	{
+		if (mOnItemUsedFunctionMap.find(mPlayerInventory[playerNo][invSlot]) != mOnItemUsedFunctionMap.end())
+		{
+			mOnItemUsedFunctionMap[mPlayerInventory[playerNo][invSlot]](playerNo);
+		}
+
+		mPlayerInventory[playerNo][invSlot] = none;
+	}
+
+	PlayerInventory::item PlayerInventory::GetRandomItemFromPool(unsigned int seed)
+	{
+		std::mt19937 rng(seed);
+		std::shuffle(itemsInRandomPool.begin(), itemsInRandomPool.end(), rng);
+		return itemsInRandomPool[0];
+	}
