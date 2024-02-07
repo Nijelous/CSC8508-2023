1af82d2f6a626268c821c2f52ef55eabda0e163e
KyriakosNU <149068759+KyriakosNU@users.noreply.github.com>
2024-02-07 11:35:56 +0000

    Player Buffs base

---
+++ CSC8503/InventoryBuffSystem/PlayerBuffs.cpp
@@ -0,0 +1,59 @@
+#include "PlayerBuffs.h"
+
+using namespace InventoryBuffSystem;
+
+void PlayerBuffs::Init()
+{
+	for (int playerNo = 0; playerNo < MAX_PLAYERS; playerNo++)
+		mActiveBuffDurationMap[playerNo].clear();
+
+	mBuffsInRandomPool =
+	{
+		disguise, buff2
+	};
+}
+
+void PlayerBuffs::ApplyBuffToPlayer(buff inBuff, int playerNo)
+{
+	mOnBuffAppliedFunctionMap[inBuff](playerNo);
+	mActiveBuffDurationMap[playerNo][inBuff] = mBuffInitDurationMap[inBuff];
+}
+
+void PlayerBuffs::RemoveBuffFromPlayer(buff inBuff, int playerNo)
+{
+	auto foundBuff = mActiveBuffDurationMap[playerNo].find(inBuff);
+
+	if (foundBuff != mActiveBuffDurationMap[playerNo].end())
+	{
+		mOnBuffRemovedFunctionMap[inBuff](playerNo);
+		mActiveBuffDurationMap[playerNo].erase(foundBuff);
+	}
+};
+
+PlayerBuffs::buff PlayerBuffs::GetRandomBuffFromPool(unsigned int seed)
+{
+	std::mt19937 rng(seed);
+	std::shuffle(mBuffsInRandomPool.begin(), mBuffsInRandomPool.end(), rng);
+	return mBuffsInRandomPool[0];
+}
+
+void PlayerBuffs::Update(float dt)
+{
+	for (int playerNo = 0; playerNo < MAX_PLAYERS; playerNo++)
+	{
+		for (auto entry = mActiveBuffDurationMap[playerNo].begin();
+			entry != mActiveBuffDurationMap[playerNo].end(); ++entry)
+		{
+			entry->second -= dt;
+
+			if (entry->second > 0)
+			{
+				mOnBuffTickFunctionMap[entry->first](playerNo, dt);
+			}
+			else
+			{
+				RemoveBuffFromPlayer(entry->first, playerNo);
+			}
+		}
+	}
+}
