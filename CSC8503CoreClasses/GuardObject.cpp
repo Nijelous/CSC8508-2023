902b7da7a068e9e84dee74b4cfd49c10951f6aef
ollieiscool <oliverperrin10@hotmail.co.uk>
2024-02-12 19:07:02 +0000

    Guard rotation
    Guard now looks at the player using a very complex method that turns a direction vector into a quaternion.

--- CSC8503CoreClasses/GuardObject.cpp
+++ CSC8503CoreClasses/GuardObject.cpp
@@ -13,7 +13,7 @@ GuardObject::GuardObject(const std::string& objectName) {
 	mRootSequence = new BehaviourSequence("Root Sequence");
 	mCanSeePlayer = false;
 	mHasCaughtPlayer = false;
-	mHasItems = true;
+	mPlayerHasItems = true;
 	BehaviourTree();
 }
 
@@ -60,6 +60,61 @@ Vector3 GuardObject::AngleOfSight() {
 	Vector3 rightAxis = this->mTransform.GetMatrix().GetColumn(0);
 	Vector3 fwdAxis = Vector3::Cross(Vector3(0, 1, 0), rightAxis);
 	return fwdAxis;
+}
+
+Quaternion GuardObject::VectorToQuaternion(Vector3 directionVector) {
+	directionVector.Normalise();
+	directionVector.y = 0;
+	Vector3 xAxis = Vector3::Cross(directionVector, Vector3(0, 1, 0));
+	xAxis.Normalise();
+	Vector3 yAxis = Vector3::Cross(directionVector, xAxis);
+	yAxis.Normalise();
+
+	Matrix3 m = Matrix3();
+	Vector3 column1 = Vector3(xAxis.x, yAxis.x, directionVector.x);
+	Vector3 column2 = Vector3(xAxis.y, yAxis.y, directionVector.y);
+	Vector3 column3 = Vector3(xAxis.z, yAxis.z, directionVector.z);
+	m.SetColumn(0, column1);
+	m.SetColumn(1, column2);
+	m.SetColumn(2, column3);
+
+	return Matrix3ToQuaternion(m);
+}
+
+Quaternion GuardObject::Matrix3ToQuaternion(Matrix3 m) {
+	Quaternion rotation = Quaternion();
+	float trace = m.GetColumn(0)[0] + m.GetColumn(1)[1] + m.GetColumn(2)[2];
+	if (trace > 0) {
+		float s = 0.5f / sqrtf(trace + 1.0f);
+		rotation.w = 0.25f / s;
+		rotation.x = (m.GetColumn(2)[1] - m.GetColumn(1)[2]) * s;
+		rotation.y = (m.GetColumn(0)[2] - m.GetColumn(2)[0]) * s;
+		rotation.z = (m.GetColumn(1)[0] - m.GetColumn(0)[1]) * s;
+	}
+	else {
+		if (m.GetColumn(0)[0] > m.GetColumn(1)[1] && m.GetColumn(0)[0] > m.GetColumn(2)[2]) {
+			float s = 2.0f * sqrtf(1.0f + m.GetColumn(0)[0] - m.GetColumn(1)[1] - m.GetColumn(2)[2]);
+			rotation.w = (m.GetColumn(2)[1] - m.GetColumn(1)[2]) / s;
+			rotation.x = 0.25f * s;
+			rotation.y = (m.GetColumn(0)[1] + m.GetColumn(1)[0]) / s;
+			rotation.z = (m.GetColumn(0)[2] + m.GetColumn(2)[0]) / s;
+		}
+		else if (m.GetColumn(1)[1] > m.GetColumn(2)[2]) {
+			float s = 2.0f * sqrtf(1.0f + m.GetColumn(1)[1] - m.GetColumn(0)[0] - m.GetColumn(2)[2]);
+			rotation.w = (m.GetColumn(0)[2] - m.GetColumn(2)[0]) / s;
+			rotation.x = (m.GetColumn(0)[1] + m.GetColumn(1)[0]) / s;
+			rotation.y = 0.25f * s;
+			rotation.z = (m.GetColumn(1)[2] + m.GetColumn(2)[1]) / s;
+		}
+		else {
+			float s = 2.0f * sqrtf(1.0f + m.GetColumn(2)[2] - m.GetColumn(0)[0] - m.GetColumn(1)[1]);
+			rotation.w = (m.GetColumn(1)[0] - m.GetColumn(0)[1]) / s;
+			rotation.x = (m.GetColumn(0)[2] + m.GetColumn(2)[0]) / s;
+			rotation.y = (m.GetColumn(1)[2] + m.GetColumn(2)[1]) / s;
+			rotation.z = 0.25f * s;
+		}
+	}
+	return rotation;
 }
 
 void GuardObject::BehaviourTree() {
@@ -110,8 +165,13 @@ BehaviourAction* GuardObject::ChasePlayerSetup() {
 		else if (state == Ongoing){
 			if (mCanSeePlayer == true && mHasCaughtPlayer == false) {
 				int GuardCatchingDistanceSquared = 25;
+				int GuardSpeedMultiplyer = 30;
 				Vector3 direction = mPlayer->GetTransform().GetPosition() - this->GetTransform().GetPosition();
-				this->GetPhysicsObject()->AddForce(Vector3(direction.x, 0, direction.z));
+				Vector3 dirNorm = direction.Normalised();
+				this->GetTransform().SetOrientation(VectorToQuaternion(direction) * Quaternion(1.0f, 0.0f,0.0f,0.0f));
+
+				this->GetPhysicsObject()->AddForce(Vector3(dirNorm.x, 0, dirNorm.z) * GuardSpeedMultiplyer);
+
 				float dist = direction.LengthSquared();
 				if (dist < GuardCatchingDistanceSquared) {
 					mHasCaughtPlayer = true;
@@ -134,27 +194,26 @@ BehaviourAction* GuardObject::ChasePlayerSetup() {
 BehaviourAction* GuardObject::ConfiscateItems() {
 	BehaviourAction* ConfiscateItems = new BehaviourAction("Confiscate Items", [&](float dt, BehaviourState state)->BehaviourState {
 		if (state == Initialise) {
-			mTimer = 60;
+			mConfiscateItemsTime = 60;
 			state = Ongoing;
 		}
 		else if (state == Ongoing) {
-			if (mCanSeePlayer == true && mHasCaughtPlayer == true && mHasItems == true) {
-				mTimer -= dt;
-				Vector3 direction = mPlayer->GetTransform().GetPosition();
-				//mPlayer->GetPhysicsObject()->AddForce(Vector3(direction.x, 0, direction.z));
-				mPlayer->GetTransform().SetPosition(Vector3(direction.x, direction.y, direction.z));
-				if (mTimer == 0) {
-					mHasItems = false;
+			if (mCanSeePlayer == true && mHasCaughtPlayer == true && mPlayerHasItems == true) {
+				mConfiscateItemsTime -= dt;
+				std::cout << "Caught";
+				Vector3 direction = this->GetTransform().GetPosition() - mPlayer->GetTransform().GetPosition();
+				mPlayer->GetPhysicsObject()->AddForce(Vector3(direction.x, 0, direction.z));
+				if (mConfiscateItemsTime == 0) {
+					mPlayerHasItems = false;
 					return Success;
 				}
 			}
-			else if (mCanSeePlayer == true && mHasCaughtPlayer == true && mHasItems == false) {
-				mTimer -= dt;
-				Vector3 direction = mPlayer->GetTransform().GetPosition();
-				//mPlayer->GetPhysicsObject()->AddForce(Vector3(direction.x, 0, direction.z));
-				mPlayer->GetTransform().SetPosition(Vector3(direction.x, direction.y, direction.z));
-				if (mTimer == 0) {
-					mHasItems = false;
+			else if (mCanSeePlayer == true && mHasCaughtPlayer == true && mPlayerHasItems == false) {
+				mConfiscateItemsTime -= dt;
+				Vector3 direction = this->GetTransform().GetPosition() - mPlayer->GetTransform().GetPosition();
+				mPlayer->GetPhysicsObject()->AddForce(Vector3(direction.x, 0, direction.z));
+				if (mConfiscateItemsTime == 0) {
+					mPlayerHasItems = false;
 					return Success;
 				}
 			}
@@ -171,12 +230,14 @@ BehaviourAction* GuardObject::ConfiscateItems() {
 BehaviourAction* GuardObject::SendToPrison() {
 	BehaviourAction* SendToPrison = new BehaviourAction("Send to Prison", [&](float dt, BehaviourState state)->BehaviourState {
 		if (state == Initialise) {
-			if (mCanSeePlayer == true && mHasCaughtPlayer == true && mHasItems == false) {
+			if (mCanSeePlayer == true && mHasCaughtPlayer == true && mPlayerHasItems == false) {
 				Vector3 prisonLocation = Vector3(mPlayer->GetTransform().GetPosition().x, 100, mPlayer->GetTransform().GetPosition().z);
 				mPlayer->GetTransform().SetPosition(prisonLocation);
+				mHasCaughtPlayer = false;
 				return Success;
 			}
 			else {
+				mHasCaughtPlayer = false;
 				return Failure;
 			}
 			state = Ongoing;
