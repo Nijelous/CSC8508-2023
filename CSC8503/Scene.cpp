365dd58425a98a7c67a46b93ea73444072c82b68
erendgrmnc <erendegirmenci.354@gmail.com>
2024-02-13 11:38:52 +0000

    feat(logic): added scene class

---
+++ CSC8503/Scene.cpp
@@ -0,0 +1,36 @@
+﻿#include "Scene.h"
+
+#include "LevelManager.h"
+
+NCL::CSC8503::Scene::Scene() : mController(*Window::GetWindow()->GetKeyboard(), *Window::GetWindow()->GetMouse()){
+    mLevelManager = std::make_unique<LevelManager>();
+    
+    mLevelManager->GetGameWorld()->GetMainCamera().SetController(mController);
+    mController.MapAxis(0, "Sidestep");
+    mController.MapAxis(1, "UpDown");
+    mController.MapAxis(2, "Forward");
+    mController.MapAxis(3, "XLook");
+    mController.MapAxis(4, "YLook");
+
+    InitCamera();
+}
+
+Scene::~Scene(){
+    
+}
+
+void Scene::UpdateGame(float dt){
+    mLevelManager->Update(dt, true);
+}
+
+std::unique_ptr<NCL::CSC8503::LevelManager> NCL::CSC8503::Scene::GetLevelManager(){
+    return std::move(mLevelManager);
+}
+
+void Scene::InitCamera(){
+    mLevelManager->GetGameWorld()->GetMainCamera().SetNearPlane(0.1f);
+    mLevelManager->GetGameWorld()->GetMainCamera().SetFarPlane(500.0f);
+    mLevelManager->GetGameWorld()->GetMainCamera().SetPitch(-15.0f);
+    mLevelManager->GetGameWorld()->GetMainCamera().SetYaw(315.0f);
+    mLevelManager->GetGameWorld()->GetMainCamera().SetPosition(Vector3(-60, 40, 60));
+}
---
+++ CSC8503/Scene.h
@@ -0,0 +1,27 @@
+﻿#include "../NCLCoreClasses/KeyboardMouseController.h"
+
+#pragma once
+#include <memory>
+
+namespace NCL{
+    namespace CSC8503{
+        class LevelManager;
+
+        class Scene{
+        public:
+            Scene();
+            ~Scene();
+
+            virtual void UpdateGame(float dt);
+
+            std::unique_ptr<LevelManager> GetLevelManager();
+
+        protected:
+            std::unique_ptr<LevelManager> mLevelManager;
+
+            virtual void InitCamera();
+
+            KeyboardMouseController mController;
+        };
+    }
+}
