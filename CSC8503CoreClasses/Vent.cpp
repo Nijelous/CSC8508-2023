59dd5a1af9d34708af8409dd917227404a411b78
erendgrmnc <erendegirmenci.354@gmail.com>
2024-02-16 16:27:30 +0000

    feat(logic): added interact interface

--- CSC8503CoreClasses/Vent.cpp
+++ CSC8503CoreClasses/Vent.cpp
@@ -11,3 +11,14 @@ void Vent::ConnectVent(Vent* vent) {
 	if (vent == nullptr) return;
 	mConnectedVent = vent;
 }
+
+void Vent::Interact() {
+	if (mIsOpen) {
+		//TODO(erendgrmnc): teleport player to the other side of the vent.
+	}
+	else {
+		//TODO(erendgrmnc): When levelManager is implemented, get local player reference and check is player can open vents.
+		ToggleOpen();
+	}
+
+}
--- CSC8503CoreClasses/Vent.h
+++ CSC8503CoreClasses/Vent.h
@@ -1,14 +1,16 @@
 #pragma once
 #include "GameObject.h"
+#include "Interactable.h"
 
 namespace NCL {
     namespace CSC8503 {
-        class Vent : public GameObject {
+        class Vent : public GameObject, public Interactable {
         public:
             Vent();
             void ConnectVent(Vent* vent);
             bool IsOpen() { return mIsOpen; }
             void ToggleOpen() { mIsOpen = !mIsOpen; }
+            void Interact() override;
         protected:
             bool mIsOpen;
             Vent* mConnectedVent;
