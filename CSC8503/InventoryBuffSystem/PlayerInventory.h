96b986f0fec3f227575b71ff09d905d78403743f
erendgrmnc <erendegirmenci.354@gmail.com>
2024-02-17 19:25:13 +0000

    feat(data): added screwdriver value to items enum

--- CSC8503/InventoryBuffSystem/PlayerInventory.h
+++ CSC8503/InventoryBuffSystem/PlayerInventory.h
@@ -14,7 +14,7 @@ namespace InventoryBuffSystem
 {
 	const enum InventoryEvent
 	{
-		flagDropped,disguiseItemUsed,soundEmitterUsed
+		flagDropped,disguiseItemUsed,soundEmitterUsed, screwdriverUsed
 	};
 
 	class PlayerInventoryObserver
@@ -31,7 +31,7 @@ namespace InventoryBuffSystem
 
 		enum item
 		{
-			none, disguise, soundEmitter, flag
+			none, disguise, soundEmitter, flag, screwdriver
 		};
 
 		PlayerInventory()
@@ -57,7 +57,7 @@ namespace InventoryBuffSystem
 
 		std::vector<item> mItemsInRandomPool =
 		{
-			disguise, soundEmitter
+			screwdriver
 		};
 
 		std::map<item, InventoryEvent > mOnItemAddedInventoryEventMap =
