df3d1849311ee506d6a283e073aca193d1cfed67
erendgrmnc <erendegirmenci.354@gmail.com>
2024-02-21 15:28:01 +0000

    refactor(code): changed handle item usage and remove

--- CSC8503/InventoryBuffSystem/PlayerInventory.h
+++ CSC8503/InventoryBuffSystem/PlayerInventory.h
@@ -43,38 +43,52 @@ namespace InventoryBuffSystem
 		void AddItemToPlayer(item inItem, int playerNo);
 		void DropItemFromPlayer(item inItem, int playerNo);
 		void DropItemFromPlayer(int playerNo, int invSlot);
-		void UseItemInPlayerSlot(int itemSlot, int playerNo);
+		void UseItemInPlayerSlot(int itemSlot, int playerNo, int itemUseCount);
 		bool ItemInPlayerInventory(item inItem, int playerNo);
+		bool HandleOnItemUsed(item item, int playerNo, int invSlot, int itemUseCount);
 
 		void Attach(PlayerInventoryObserver* observer);
 		void Detach(PlayerInventoryObserver* observer);
-		void Notify(InventoryEvent invEvent,int playerNo);
+		void Notify(InventoryEvent invEvent, int playerNo, int invSLot, bool isItemRemoved = false);
+
+		std::string& GetItemName(item item);
 
 		PlayerInventory::item GetRandomItemFromPool(unsigned int seed);
 		PlayerInventory::item GetPlayerItem(int playerId, int itemSlot);
 
 	private:
 
-		std::vector<item> mItemsInRandomPool =
-		{
+		std::vector<item> mItemsInRandomPool = {
 			screwdriver
 		};
 
-		std::map<item, InventoryEvent > mOnItemAddedInventoryEventMap =
-		{
+		std::map<item, InventoryEvent > mOnItemAddedInventoryEventMap = {
 
 		};
 
-		std::map<item, InventoryEvent > mOnItemDroppedInventoryEventMap =
-		{
+		std::map<item, InventoryEvent > mOnItemDroppedInventoryEventMap = {
 			{flag,flagDropped}
 		};
 
-		std::map<item, InventoryEvent > mOnItemUsedInventoryEventMap =
-		{
-			{disguise, disguiseItemUsed}, {soundEmitter, soundEmitterUsed}
+		std::map<item, InventoryEvent > mOnItemUsedInventoryEventMap = {
+			{disguise, disguiseItemUsed},
+			{soundEmitter, soundEmitterUsed},
+			{screwdriver, screwdriverUsed }
 		};
-		
+
+		std::map<item, std::string> mItemNameMap = {
+			{ screwdriver, "Screwdriver" },
+			{ disguise, "Disguise" },
+			{ soundEmitter, "Sound Emitter" },
+			{ none, "No Equipped Item" }
+		};
+
+		std::map<item, int> mItemUsageToRemoveMap = {
+			{ screwdriver, 2 },
+			{ disguise, 1 },
+			{ soundEmitter, 1 }
+		};
+
 		std::map<item, std::function<bool(int playerNo)>> mItemPreconditionsMet;
 
 		item mPlayerInventory[NCL::CSC8503::MAX_PLAYERS][MAX_INVENTORY_SLOTS];
