49b3b12f9c9f037dbefcdf5f920157e6a8e53055
KyriakosNU <149068759+KyriakosNU@users.noreply.github.com>
2024-02-26 19:36:01 +0000

    I guess thats not enough

--- CSC8503/LevelManager.h
+++ CSC8503/LevelManager.h
@@ -14,7 +14,7 @@ using namespace SuspicionSystem;
 namespace NCL {
 	constexpr float PLAYER_MESH_SIZE = 3.0f;
 	constexpr float PLAYER_INVERSE_MASS = 0.5f;
-	constexpr float INIT_TIMER_VALUE = 10;
+	constexpr float INIT_TIMER_VALUE = 1000;
 	namespace CSC8503 {
 		class PlayerObject;
 		class GuardObject;
