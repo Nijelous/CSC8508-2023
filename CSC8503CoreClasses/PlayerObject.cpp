584d72fb57d6dd78c912a8bef7759c1a9d7b255d
Ewan Squire <squiree98@outlook.com>
2024-02-26 14:20:19 +0000

    Chnaged variable name

--- CSC8503CoreClasses/PlayerObject.cpp
+++ CSC8503CoreClasses/PlayerObject.cpp
@@ -14,7 +14,7 @@
 using namespace NCL::CSC8503;
 
 namespace {
-	constexpr float MINIMUM_SPEED = 2.f;
+	constexpr float STOPPING_SPEED = 3.f;
 
 	constexpr float CHAR_STANDING_HEIGHT = 1.4f;
 	constexpr float CHAR_CROUCH_HEIGHT = .7f;
@@ -596,7 +596,7 @@ void NCL::CSC8503::PlayerObject::ResetEquippedItemUsageCount(int inventorySlot) 
 }
 
 void PlayerObject::StopSliding() {
-	if ((mPhysicsObject->GetLinearVelocity().Length() < MINIMUM_SPEED) && (mPhysicsObject->GetForce() == Vector3(0, 0, 0))) {
+	if ((mPhysicsObject->GetLinearVelocity().Length() < STOPPING_SPEED) && (mPhysicsObject->GetForce() == Vector3(0, 0, 0))) {
 		float fallingSpeed = mPhysicsObject->GetLinearVelocity().y;
 		mPhysicsObject->SetLinearVelocity(Vector3(0, fallingSpeed, 0));
 	}
