dbd358ef92f5f72024c2c79bfed68cce368f5586
Chris Chen <c3024957@newcastle.ac.uk>
2024-02-27 14:20:28 +0000

    1

--- CSC8503CoreClasses/AnimationSystem.cpp
+++ CSC8503CoreClasses/AnimationSystem.cpp
@@ -118,7 +118,7 @@ void AnimationSystem::UpdateAnimations(std::map<std::string, MeshAnimation*> pre
 	for (auto& obj : mPlayerList) {
 
 		GameObject::GameObjectState mObjectState = obj->GetGameOjbectState();
-		std::cout << mObjectState << std::endl;
+
 		if (mPlayerState != mObjectState) {
 			mPlayerState = (GameObject::GameObjectState)mObjectState;
 			obj->GetAnimationObject()->ReSetCurrentFrame();
