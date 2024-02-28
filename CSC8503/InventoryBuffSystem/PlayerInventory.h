9b82110875b0e75d2cc78137c28372248fbed497
KyriakosNU <149068759+KyriakosNU@users.noreply.github.com>
2024-02-28 17:28:22 +0000

    Fixed bug with interactables and item usage

--- CSC8503/InventoryBuffSystem/PlayerInventory.h
+++ CSC8503/InventoryBuffSystem/PlayerInventory.h
@@ -83,7 +83,7 @@ namespace InventoryBuffSystem
 	private:
 
 		std::vector<item> mItemsInSingleplayerRandomPool = {
-			doorKey
+			doorKey, screwdriver
 		};
 
 		std::vector<item> mItemsInMultiplayerRandomPool = {
