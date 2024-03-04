54de05a496d853393d429424b52826791cade0cd
erendgrmnc <erendegirmenci.354@gmail.com>
2024-03-04 17:18:22 +0000

    refactor(logic): changed helipad player check func

--- CSC8503CoreClasses/Helipad.cpp
+++ CSC8503CoreClasses/Helipad.cpp
@@ -5,17 +5,19 @@
 using namespace NCL::CSC8503;
 
 Helipad::Helipad() : GameObject(StaticObj, "Helipad") {
-
+	mCollidingPlayerID = -1;
 }
 
 void Helipad::OnCollisionBegin(GameObject* otherObject) {
 	if (PlayerObject* temp = dynamic_cast<PlayerObject*>(otherObject)) {
 		mCollidingWithPlayer = true;
+		mCollidingPlayerID = temp->GetPlayerID();
 	}
 }
 
 void Helipad::OnCollisionEnd(GameObject* otherObject) {
 	if (PlayerObject* temp = dynamic_cast<PlayerObject*>(otherObject)) {
 		mCollidingWithPlayer = false;
+		mCollidingPlayerID = -1;
 	}
 }
