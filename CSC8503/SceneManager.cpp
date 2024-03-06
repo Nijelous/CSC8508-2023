a6b42b052ae4a84e576530910bd5400bfa646010
Ewan Squire <c0051039@newcastle.ac.uk>
2024-03-06 11:36:13 +0000

    Removed scene until PS5 window has been managed

--- CSC8503/SceneManager.cpp
+++ CSC8503/SceneManager.cpp
@@ -3,6 +3,8 @@
 #include "DebugNetworkedGame.h"
 #include "MainMenuScene.h"
 #include "SceneStates.h"
+#include "GameSceneManager.h"
+
 
 using namespace NCL::CSC8503;
 
@@ -18,16 +20,20 @@ SceneManager::~SceneManager() {
 }
 
 void SceneManager::InitScenes() {
-	auto* singlePlayerScene = new GameSceneManager();
-	auto* mainMenuScene = new MainMenuScene();
-	auto* multiplayerScene = new DebugNetworkedGame();
+	GameSceneManager* singlePlayerScene = new GameSceneManager();
+	MainMenuScene* mainMenuScene = new MainMenuScene();
+#ifdef USEGL
+	DebugNetworkedGame* multiplayerScene = new DebugNetworkedGame();
+#endif
 
 	mCurrentSceneType = Scenes::MainMenu;
 
 	gameScenesMap =
 	{
 		{Scenes::Singleplayer, (Scene*)singlePlayerScene},
+#ifdef USEGL
 		{Scenes::Multiplayer, (Scene*)multiplayerScene},
+#endif
 		{Scenes::MainMenu, (Scene*)mainMenuScene}
 	};
 }
@@ -72,7 +78,7 @@ Scene* SceneManager::GetCurrentScene() {
 }
 
 SceneManager* SceneManager::GetSceneManager() {
-	if (instance == nullptr){
+	if (instance == nullptr) {
 		instance = new SceneManager();
 	}
 	return instance;
