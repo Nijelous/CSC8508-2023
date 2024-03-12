2cfde0ffe6f4691d0c174590e49e76950221d5b1
consideri <72757488+fm19121@users.noreply.github.com>
2024-03-12 15:40:53 +0000

    e

--- CSC8503CoreClasses/InteractableDoor.cpp
+++ CSC8503CoreClasses/InteractableDoor.cpp
@@ -16,6 +16,7 @@ InteractableDoor::InteractableDoor() {
 
 void InteractableDoor::Unlock() {
 	mIsLocked = false;
+	this->GetSoundObject()->LockDoorTriggered();
 	mLockCooldown = initLockCooldown;
 	SetNavMeshFlags(2);
 }
